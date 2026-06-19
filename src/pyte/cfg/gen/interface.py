a395344001e5659a5e62a05b3d084cae3fa514dd
Konstantin Ushakov <konstantin.ushakov@oktet.tech>
2026-06-19 16:46:51 +0000

    lib/pyte: check in generated cfg sys and interface modules
    Signed-off-by: Konstantin Ushakov <konstantin.ushakov@oktet.tech>

---
+++ src/pyte/cfg/gen/interface.py
@@ -0,0 +1,287 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2026 Konstantin Ushakov
+# DO NOT EDIT - generated from TE CM YAML by pyte.cfg._gen.
+"""Network interface"""
+from pyte.cfg import (
+    AddrKnob,
+    BoolKnob,
+    CfgObject,
+    Collection,
+    IntKnob,
+    SelfKnob,
+    StrKnob,
+    SubObject,
+)
+
+
+class NetAddr(CfgObject):
+    """Network address (IPv4 or IPv6) of the interface."""
+    value = SelfKnob(cvt_name="INT32")
+    broadcast = AddrKnob("broadcast")
+
+
+class McastLinkAddr(CfgObject):
+    """Multicasting MAC address."""
+    pass
+
+
+class Hwtstamp(CfgObject):
+    """Hardware timestamping configuration."""
+    tx_type = StrKnob("tx_type")
+    rx_filter = StrKnob("rx_filter")
+
+
+class NeighStatic(CfgObject):
+    """Static neighbour entries."""
+    value = SelfKnob(cvt_name="ADDRESS")
+
+
+class NeighDynamic(CfgObject):
+    """Dynamic neighbour entries."""
+    value = SelfKnob(cvt_name="ADDRESS")
+    state = IntKnob("state", cvt_name="INT32", access="read_only")
+
+
+class NeighProxy(CfgObject):
+    """Neighbour proxy entries."""
+    pass
+
+
+class Stats(CfgObject):
+    """Interface statistics."""
+    value = SelfKnob(cvt_name="STRING")
+    in_octets = IntKnob("in_octets", cvt_name="UINT64", access="read_only")
+    in_ucast_pkts = IntKnob("in_ucast_pkts", cvt_name="UINT64", access="read_only")
+    in_nucast_pkts = IntKnob("in_nucast_pkts", cvt_name="UINT64", access="read_only")
+    in_discards = IntKnob("in_discards", cvt_name="UINT64", access="read_only")
+    in_errors = IntKnob("in_errors", cvt_name="UINT64", access="read_only")
+    in_unknown_protos = IntKnob("in_unknown_protos", cvt_name="UINT64", access="read_only")
+    out_octets = IntKnob("out_octets", cvt_name="UINT64", access="read_only")
+    out_ucast_pkts = IntKnob("out_ucast_pkts", cvt_name="UINT64", access="read_only")
+    out_nucast_pkts = IntKnob("out_nucast_pkts", cvt_name="UINT64", access="read_only")
+    out_discards = IntKnob("out_discards", cvt_name="UINT64", access="read_only")
+    out_errors = IntKnob("out_errors", cvt_name="UINT64", access="read_only")
+
+
+class Vlans(CfgObject):
+    """Network interface VLANs"""
+    value = SelfKnob(cvt_name="STRING")
+    ifname = StrKnob("ifname", access="read_only")
+
+
+class Macvlan(CfgObject):
+    """Virtual interface based on link layer address (MAC)"""
+    value = SelfKnob(cvt_name="STRING")
+
+
+class Ipvlan(CfgObject):
+    """Virtual IP VLAN interface"""
+    value = SelfKnob(cvt_name="STRING")
+
+
+class CoalesceGlobalParam(CfgObject):
+    value = SelfKnob(cvt_name="UINT64")
+
+
+class CoalesceGlobal(CfgObject):
+    """Interrupt coalescing parameters at interface level"""
+    param = Collection("param", CoalesceGlobalParam)
+
+
+class CoalesceQueuesQueueParam(CfgObject):
+    value = SelfKnob(cvt_name="UINT64")
+
+
+class CoalesceQueuesQueue(CfgObject):
+    """Interrupt coalescing parameters for a specific queue"""
+    param = Collection("param", CoalesceQueuesQueueParam)
+
+
+class CoalesceQueues(CfgObject):
+    """Interrupt coalescing parameters for network interface queues"""
+    queue = Collection("queue", CoalesceQueuesQueue)
+
+
+class Coalesce(CfgObject):
+    """Network interface interrupt coalescing parameters"""
+    global_ = SubObject("global", CoalesceGlobal)
+    queues = SubObject("queues", CoalesceQueues)
+
+
+class FlowControl(CfgObject):
+    """Network interface flow control parameters"""
+    autoneg = IntKnob("autoneg", cvt_name="INT32")
+    rx = IntKnob("rx", cvt_name="INT32")
+    tx = IntKnob("tx", cvt_name="INT32")
+
+
+class PhyMode(CfgObject):
+    """Supported link mode"""
+    value = SelfKnob(cvt_name="INT32")
+
+
+class PhyLpAdvertised(CfgObject):
+    """Link mode advertised by link partner"""
+    pass
+
+
+class Phy(CfgObject):
+    """Network interface PHY parameters"""
+    port = StrKnob("port", access="read_only")
+    autoneg = IntKnob("autoneg", cvt_name="INT32")
+    duplex_admin = StrKnob("duplex_admin")
+    duplex_oper = StrKnob("duplex_oper", access="read_only")
+    speed_admin = IntKnob("speed_admin", cvt_name="INT32")
+    speed_oper = IntKnob("speed_oper", cvt_name="INT32", access="read_only")
+    mode = Collection("mode", PhyMode)
+    lp_advertised = Collection("lp_advertised", PhyLpAdvertised)
+    state = IntKnob("state", cvt_name="INT32", access="read_only")
+    set_supported = IntKnob("set_supported", cvt_name="INT32", access="read_only")
+
+
+class Eee(CfgObject):
+    """Network interface Energy Efficient Ethernet parameters"""
+    eee_active = BoolKnob("eee_active", access="read_only")
+    eee_enabled = BoolKnob("eee_enabled")
+    tx_lpi_enabled = IntKnob("tx_lpi_enabled", cvt_name="UINT64")
+    tx_lpi_timer = IntKnob("tx_lpi_timer", cvt_name="UINT32")
+
+
+class Feature(CfgObject):
+    """Network interface features"""
+    value = SelfKnob(cvt_name="INT32")
+    readonly = IntKnob("readonly", cvt_name="INT32", access="read_only")
+
+
+class PrivateFlag(CfgObject):
+    """Network interface private flag"""
+    value = SelfKnob(cvt_name="BOOL")
+
+
+class Private(CfgObject):
+    """Network interface private flags"""
+    flag = Collection("flag", PrivateFlag)
+
+
+class ChannelsCombined(CfgObject):
+    """NIC combined channel count controls"""
+    current = IntKnob("current", cvt_name="INT32")
+    maximum = IntKnob("maximum", cvt_name="INT32", access="read_only")
+
+
+class ChannelsOther(CfgObject):
+    """NIC other channel count controls"""
+    current = IntKnob("current", cvt_name="INT32")
+    maximum = IntKnob("maximum", cvt_name="INT32", access="read_only")
+
+
+class ChannelsRx(CfgObject):
+    """NIC Rx channel count controls"""
+    current = IntKnob("current", cvt_name="INT32")
+    maximum = IntKnob("maximum", cvt_name="INT32", access="read_only")
+
+
+class ChannelsTx(CfgObject):
+    """NIC Tx channel count controls"""
+    current = IntKnob("current", cvt_name="INT32")
+    maximum = IntKnob("maximum", cvt_name="INT32", access="read_only")
+
+
+class Channels(CfgObject):
+    """NIC channel count controls"""
+    combined = SubObject("combined", ChannelsCombined)
+    other = SubObject("other", ChannelsOther)
+    rx = SubObject("rx", ChannelsRx)
+    tx = SubObject("tx", ChannelsTx)
+
+
+class RingRx(CfgObject):
+    """NIC Rx ring size configuration."""
+    max = IntKnob("max", cvt_name="INT64", access="read_only")
+    current = IntKnob("current", cvt_name="INT64")
+
+
+class RingTx(CfgObject):
+    """NIC Tx ring size configuration."""
+    max = IntKnob("max", cvt_name="INT64", access="read_only")
+    current = IntKnob("current", cvt_name="INT64")
+
+
+class Ring(CfgObject):
+    """NIC ring size configuration."""
+    rx = SubObject("rx", RingRx)
+    tx = SubObject("tx", RingTx)
+
+
+class Deviceinfo(CfgObject):
+    """Network interface info"""
+    drivername = StrKnob("drivername", access="read_only")
+    driverversion = StrKnob("driverversion", access="read_only")
+    firmwareversion = StrKnob("firmwareversion", access="read_only")
+
+
+class XstatsXstat(CfgObject):
+    """Network interface certain extended statistic"""
+    value = SelfKnob(cvt_name="UINT64")
+
+
+class Xstats(CfgObject):
+    """Network interface extended statistics"""
+    xstat = Collection("xstat", XstatsXstat)
+
+
+class IrqCpu(CfgObject):
+    """Interrupt counter per CPU"""
+    value = SelfKnob(cvt_name="UINT64")
+
+
+class Irq(CfgObject):
+    """Network interface interrupt information"""
+    name = StrKnob("name", access="read_only")
+    cpu = Collection("cpu", IrqCpu)
+    smp_affinity = IntKnob("smp_affinity", cvt_name="UINT64")
+
+
+class Interface(CfgObject):
+    """Network interface"""
+    switch_id = StrKnob("switch_id", access="read_only")
+    port_id = StrKnob("port_id", access="read_only")
+    port_name = StrKnob("port_name", access="read_only")
+    index = IntKnob("index", cvt_name="INT32", access="read_only")
+    net_addr = Collection("net_addr", NetAddr)
+    mcast_link_addr = Collection("mcast_link_addr", McastLinkAddr)
+    link_addr = AddrKnob("link_addr")
+    bcast_link_addr = AddrKnob("bcast_link_addr")
+    mtu = IntKnob("mtu", cvt_name="INT32")
+    ip4_ttl = IntKnob("ip4_ttl", cvt_name="INT32")
+    status = IntKnob("status", cvt_name="INT32")
+    oper_status = IntKnob("oper_status", cvt_name="INT32", access="read_only")
+    hwtstamp = SubObject("hwtstamp", Hwtstamp)
+    promisc = IntKnob("promisc", cvt_name="INT32")
+    allmulti = BoolKnob("allmulti")
+    arp = IntKnob("arp", cvt_name="INT32")
+    neigh_static = Collection("neigh_static", NeighStatic)
+    neigh_dynamic = Collection("neigh_dynamic", NeighDynamic)
+    neigh_proxy = Collection("neigh_proxy", NeighProxy)
+    stats = SubObject("stats", Stats)
+    vlans = Collection("vlans", Vlans)
+    macvlan = Collection("macvlan", Macvlan)
+    ipvlan = Collection("ipvlan", Ipvlan)
+    parent = StrKnob("parent", access="read_only")
+    kind = StrKnob("kind", access="read_only")
+    coalesce = SubObject("coalesce", Coalesce)
+    flow_control = SubObject("flow_control", FlowControl)
+    phy = SubObject("phy", Phy)
+    eee = SubObject("eee", Eee)
+    feature = Collection("feature", Feature)
+    private = SubObject("private", Private)
+    channels = SubObject("channels", Channels)
+    ring = SubObject("ring", Ring)
+    msglvl = IntKnob("msglvl", cvt_name="UINT64")
+    deviceinfo = SubObject("deviceinfo", Deviceinfo)
+    reset = IntKnob("reset", cvt_name="INT32")
+    xstats = SubObject("xstats", Xstats)
+    irq = Collection("irq", Irq)
+
+    def __init__(self, ta, ifname):
+        super().__init__(f"/agent:{ta}/interface:{ifname}")
---
+++ src/pyte/cfg/gen/sys.py
@@ -0,0 +1,148 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2026 Konstantin Ushakov
+# DO NOT EDIT - generated from TE CM YAML by pyte.cfg._gen.
+"""System settings from /proc/sys/."""
+from pyte.cfg import (
+    CfgObject,
+    IntKnob,
+    StrKnob,
+    SubObject,
+)
+
+
+class Vm(CfgObject):
+    """System settings from /proc/sys/vm."""
+    nr_hugepages = IntKnob("nr_hugepages", cvt_name="INT32")
+    overcommit_memory = IntKnob("overcommit_memory", cvt_name="INT32")
+
+
+class Fs(CfgObject):
+    """System settings from /proc/sys/fs."""
+    file_max = IntKnob("file-max", cvt_name="UINT64")
+    file_nr = IntKnob("file-nr", cvt_name="UINT64", access="read_only")
+
+
+class NetCore(CfgObject):
+    """System settings from /proc/sys/net/core/."""
+    busy_read = IntKnob("busy_read", cvt_name="INT32")
+    busy_poll = IntKnob("busy_poll", cvt_name="INT32")
+    somaxconn = IntKnob("somaxconn", cvt_name="INT32")
+    wmem_default = IntKnob("wmem_default", cvt_name="INT32")
+    wmem_max = IntKnob("wmem_max", cvt_name="INT32")
+    rmem_default = IntKnob("rmem_default", cvt_name="INT32")
+    rmem_max = IntKnob("rmem_max", cvt_name="INT32")
+    optmem_max = IntKnob("optmem_max", cvt_name="INT32")
+
+
+class NetNetfilter(CfgObject):
+    """System settings from /proc/sys/net/netfilter/."""
+    nf_conntrack_max = IntKnob("nf_conntrack_max", cvt_name="INT32")
+    nf_conntrack_tcp_loose = IntKnob("nf_conntrack_tcp_loose", cvt_name="INT32")
+
+
+class NetIpv4Conf(CfgObject):
+    """System settings from /proc/sys/net/ipv4/conf/."""
+    forwarding = IntKnob("forwarding", cvt_name="INT32")
+    rp_filter = IntKnob("rp_filter", cvt_name="INT32")
+    arp_ignore = IntKnob("arp_ignore", cvt_name="INT32")
+    proxy_arp = IntKnob("proxy_arp", cvt_name="INT32")
+
+
+class NetIpv4Neigh(CfgObject):
+    """System settings from /proc/sys/net/ipv4/neigh/."""
+    gc_thresh3 = IntKnob("gc_thresh3", cvt_name="INT32")
+
+
+class NetIpv4Route(CfgObject):
+    """System settings from /proc/sys/net/ipv4/route/."""
+    mtu_expires = IntKnob("mtu_expires", cvt_name="INT32")
+
+
+class NetIpv4(CfgObject):
+    """System settings from /proc/sys/net/ipv4/."""
+    ip_forward = IntKnob("ip_forward", cvt_name="INT32")
+    ipfrag_max_dist = IntKnob("ipfrag_max_dist", cvt_name="INT32")
+    ipfrag_high_thresh = IntKnob("ipfrag_high_thresh", cvt_name="INT32")
+    tcp_timestamps = IntKnob("tcp_timestamps", cvt_name="INT32")
+    tcp_syncookies = IntKnob("tcp_syncookies", cvt_name="INT32")
+    tcp_max_syn_backlog = IntKnob("tcp_max_syn_backlog", cvt_name="INT32")
+    tcp_keepalive_time = IntKnob("tcp_keepalive_time", cvt_name="INT32")
+    tcp_keepalive_probes = IntKnob("tcp_keepalive_probes", cvt_name="INT32")
+    tcp_keepalive_intvl = IntKnob("tcp_keepalive_intvl", cvt_name="INT32")
+    tcp_retries2 = IntKnob("tcp_retries2", cvt_name="INT32")
+    tcp_orphan_retries = IntKnob("tcp_orphan_retries", cvt_name="INT32")
+    tcp_syn_retries = IntKnob("tcp_syn_retries", cvt_name="INT32")
+    tcp_synack_retries = IntKnob("tcp_synack_retries", cvt_name="INT32")
+    tcp_fin_timeout = IntKnob("tcp_fin_timeout", cvt_name="INT32")
+    tcp_wmem = IntKnob("tcp_wmem", cvt_name="INT32")
+    tcp_rmem = IntKnob("tcp_rmem", cvt_name="INT32")
+    tcp_sack = IntKnob("tcp_sack", cvt_name="INT32")
+    tcp_dsack = IntKnob("tcp_dsack", cvt_name="INT32")
+    igmp_max_memberships = IntKnob("igmp_max_memberships", cvt_name="INT32")
+    tcp_early_retrans = IntKnob("tcp_early_retrans", cvt_name="INT32")
+    fib_multipath_hash_policy = IntKnob("fib_multipath_hash_policy", cvt_name="INT32")
+    ip_default_ttl = IntKnob("ip_default_ttl", cvt_name="INT32")
+    tcp_congestion_control = StrKnob("tcp_congestion_control")
+    icmp_ratelimit = IntKnob("icmp_ratelimit", cvt_name="INT32")
+    conf = SubObject("conf", NetIpv4Conf)
+    neigh = SubObject("neigh", NetIpv4Neigh)
+    route = SubObject("route", NetIpv4Route)
+
+
+class NetIpv6Neigh(CfgObject):
+    pass
+
+
+class NetIpv6Conf(CfgObject):
+    """System settings from /proc/sys/net/ipv6/conf/."""
+    disable_ipv6 = IntKnob("disable_ipv6", cvt_name="INT32")
+    forwarding = IntKnob("forwarding", cvt_name="INT32")
+    proxy_ndp = IntKnob("proxy_ndp", cvt_name="INT32")
+    keep_addr_on_down = IntKnob("keep_addr_on_down", cvt_name="INT32")
+    hop_limit = IntKnob("hop_limit", cvt_name="INT32")
+
+
+class NetIpv6Route(CfgObject):
+    mtu_expires = IntKnob("mtu_expires", cvt_name="INT32")
+
+
+class NetIpv6(CfgObject):
+    neigh = SubObject("neigh", NetIpv6Neigh)
+    auto_flowlabels = IntKnob("auto_flowlabels", cvt_name="INT32")
+    conf = SubObject("conf", NetIpv6Conf)
+    route = SubObject("route", NetIpv6Route)
+    fib_multipath_hash_policy = IntKnob("fib_multipath_hash_policy", cvt_name="INT32")
+
+
+class Net(CfgObject):
+    """System settings from /proc/sys/net/.
+
+    Objects corresponding to files under /proc/sys/net.
+
+    To make particular parameter from /proc/sys/net/* paths
+    available, you should also register an object named after
+    that parameter under appropriate object corresponding
+    to a directory.
+    """
+    core = SubObject("core", NetCore)
+    netfilter = SubObject("netfilter", NetNetfilter)
+    ipv4 = SubObject("ipv4", NetIpv4)
+    ipv6 = SubObject("ipv6", NetIpv6)
+
+
+class Debug(CfgObject):
+    """System settings from /proc/sys/debug."""
+    exception_trace = IntKnob("exception-trace", cvt_name="INT32")
+
+
+class Sys(CfgObject):
+    """System settings from /proc/sys/."""
+    console_loglevel = IntKnob("console_loglevel", cvt_name="INT32")
+    core_pattern = StrKnob("core_pattern")
+    vm = SubObject("vm", Vm)
+    fs = SubObject("fs", Fs)
+    net = SubObject("net", Net)
+    debug = SubObject("debug", Debug)
+
+    def __init__(self, ta):
+        super().__init__(f"/agent:{ta}/sys:")
