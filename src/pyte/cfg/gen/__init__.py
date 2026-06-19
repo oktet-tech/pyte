fa9c53e20edbf5f405f5ca3d0de644105d544634
Konstantin Ushakov <konstantin.ushakov@oktet.tech>
2026-06-19 14:34:03 +0000

    lib/pyte: add PyYAML dev dep and cfg.gen package
    Signed-off-by: Konstantin Ushakov <konstantin.ushakov@oktet.tech>

---
+++ src/pyte/cfg/gen/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2026 Konstantin Ushakov
+"""Generated Configurator domain modules (produced by pyte.cfg._gen).
+
+Modules here are emitted from TE's CM YAML and checked in; edit the
+generator, not these files.
+"""
